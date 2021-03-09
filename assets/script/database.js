e01a2027cad09dcc579539647d5ab45caee937d7
mychal <mychal9251@gmail.com>
2021-03-08 20:46:21 -0700

    database JS

--- assets/script/database.js
+++ assets/script/database.js
@@ -1,3 +1,17 @@
+// var searchBtn = document.getElementById("searchBtn");
+// searchBtn.addEventListener("click", dataSearch);
+
+// function dataSearch (event) {
+//     event.preventDefault();
+//     var searchEl = document.getElementById("searchEl");
+//     var searchEl = searchEl.value;
+//     console.log(searchEl);
+// }
+
+var url0 = "https://images-api.nasa.gov/";
+var url1 = "/search?q="
+
+
 var searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", dataSearch);
 
@@ -6,4 +20,8 @@ function dataSearch (event) {
     var searchEl = document.getElementById("searchEl");
     var searchEl = searchEl.value;
     console.log(searchEl);
+
+    var searchUrl = url0 + url1 + searchEl;
+    fetch(searchUrl);
+    console.log(searchUrl);
 }
