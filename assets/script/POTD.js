3ada097e4351dcd5b1759960f60e02cad4969ba3
Mike Rhiney <rhineyman@gmail.com>
2021-03-09 18:37:04 -0800

    default for potd

--- assets/script/POTD.js
+++ assets/script/POTD.js
@@ -15,6 +15,13 @@ window.addEventListener("load", function(){
         document.getElementById("container").textContent = data.title;
         document.getElementById("date").textContent = data.date;
         document.getElementById("picture").setAttribute("src", data.url);
+        if (data.media_type !=="video"){
+            document.getElementById("picture").setAttribute("src", data.url);
+
+        }else {
+            document.getElementById("picture").setAttribute("src", "./assets/images/milkyway.jpg");
+
+        }
         document.getElementById("author").textContent = data.explanation;
 
     })
